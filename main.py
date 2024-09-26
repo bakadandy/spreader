fdeca2b3a82b800007f2c8dc40541260666d6813
bakadandy <dimash.bakytzhanov@gmail.com>
2024-09-26 15:58:54 +0500

    added fuctions to DB manage

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-#osy zherden bastaisyn
-print('start')
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
+    conn.commit()
+    conn.close()
+
+create_table()
