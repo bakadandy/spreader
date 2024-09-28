664fd896fcfb99c0c29128d5fc08a779c488fdb1
T_E_N <t_e_n_n@mail.ru>
2024-09-28 15:50:29 +0600

    added destructor in db manager

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#import sqlite3
 import sys
 import GUI
 from PyQt5.QtWidgets import QApplication
@@ -7,11 +6,3 @@ if __name__ == '__main__':
     window = GUI.LoginWindow()
     window.show()
     sys.exit(app.exec_())
-# def create_table():
-#     conn = sqlite3.connect("users.db")
-#     cur = conn.cursor()
-#     cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
-#     conn.commit()
-#     conn.close()
-#
-# create_table()
