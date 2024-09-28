868420fdd6d76344b97753445c7f09975cd245dc
T_E_N <t_e_n_n@mail.ru>
2024-09-28 15:48:39 +0600

    now gui launches from main

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import sqlite3
-
-def create_table():
-    conn = sqlite3.connect("users.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
-    conn.commit()
-    conn.close()
-
-create_table()
+#import sqlite3
+import sys
+import GUI
+from PyQt5.QtWidgets import QApplication
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GUI.LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
+# def create_table():
+#     conn = sqlite3.connect("users.db")
+#     cur = conn.cursor()
+#     cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
+#     conn.commit()
+#     conn.close()
+#
+# create_table()
