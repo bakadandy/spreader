14d524f7c9840f99c9896b5a014bf79ea40c9f2e
T_E_N <t_e_n_n@mail.ru>
2024-09-28 18:33:44 +0600

    1

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-#osy zherden bastaisyn
-print('start')
+import sys
+import GUI
+from PyQt5.QtWidgets import QApplication
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GUI.LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
