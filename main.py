8f0ed57c79654bd405fbbd1985961300806d41fc
bakadandy <dimash.bakytzhanov@gmail.com>
2024-09-28 17:21:49 +0500

    added training window

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import GUI
-from PyQt5.QtWidgets import QApplication
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = GUI.LoginWindow()
-    window.show()
-    sys.exit(app.exec_())
